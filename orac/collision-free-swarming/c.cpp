75e5ee1500999c0664c2a1df4eba498247f36a68
x5ilky <ethancai920@gmail.com>
2026-02-05 11:44:03 +1100

    oml this impl is ass

--- orac/collision-free-swarming/c.cpp
+++ orac/collision-free-swarming/c.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 #include <map>
 
@@ -14,10 +15,10 @@ int main() {
     int X = xpos_to_coord.size();
     int Y = ypos_to_coord.size();
 
-    vector<vector<int>> exists_cols(X+1), exists_rows(Y+1);
+    vector<vector<int>> y_per_col(X+1), x_per_row(Y+1);
+    vector<pii> cells;
     {
         set<int> xs, ys;
-        vector<pii> cells;
         for (int i = 1; i <= N; i++) {
             int r, c; cin >> r >> c;
             xs.insert(c);
@@ -31,11 +32,36 @@ int main() {
         for (auto& cell : cells) {
             cell.first = xpos_to_coord[cell.first];
             cell.second = ypos_to_coord[cell.second];
-            exists_cols[cell.first].push_back(cell.second);
-            exists_rows[cell.second].push_back(cell.first);
+            y_per_col[cell.first].push_back(cell.second);
+            x_per_row[cell.second].push_back(cell.first);
         }
     }
 
-    map<int, int> col_top, col_bot, row_start, row_end;
-    
+    map<int, int> col_up, col_down, row_left, row_right;
+#define chexists(col, f, fn, v) col[f] = col.count(f) ? fn(col[f], (v)) : (v)
+    for (auto& u : cells) {
+        auto [r, c] = u;
+        for (auto& v : cells) {
+            if (u == v) continue;
+            auto [r2, c2] = v;
+
+            if ((r2+c2)%2 != (r+c)%2) continue;
+            if (c2 < c) continue;
+            if (r2 < r) continue;
+            if (r == r2) {
+                int mid = (coord_to_xpos[c] + coord_to_xpos[c2])/2;
+                int ts = coord_to_ypos[r];
+                chexists(col_up, mid, max, ts-1);
+                chexists(col_down, mid, min, ts+1);
+                continue;
+            }
+            else if (c == c2) {
+                int mid = (coord_to_ypos[r] + coord_to_ypos[r2])/2;
+                int ts = coord_to_xpos[c];
+                chexists(row_right, mid, max, ts-1);
+                chexists(row_left, mid, min, ts+1);
+                continue;
+            }
+        }
+    }
 }
