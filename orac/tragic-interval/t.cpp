5d3661a654bbf0cf16ad2a88c39794e7dd2eda23
x5ilky <ethancai920@gmail.com>
2026-03-02 21:26:51 +1100

    begin potw1, im a bit slow

---
+++ orac/tragic-interval/t.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+#include <ios>
+
+using namespace std;
+// WIP
+
+const int INF=1e9+7;
+struct segtree {
+    vector<int> mn; vector<int> mx;
+    segtree(int n) : mn(4*n), mx(4*n) {
+    }
+
+    void update(int v, int tl, int tr, int pos, int a) {
+        if (tl==tr){
+            mn[v]=a;
+            mx[v]=a;
+        } else {
+            int tm=(tl+tr)/2;
+            if (pos<=tm)
+                update(v*2,tl,tm,pos,a);
+            if (pos> tm)
+                update(v*2+1,tm+1,tr,pos,a);
+            mn[v]=min(mn[v*2],mn[v*2+1]);
+            mx[v]=max(mx[v*2],mx[v*2+1]);
+        }
+    }
+    int query_min(int v, int tl, int tr, int ql, int qr) {
+        if (ql<=tl&&tr<=qr){
+            return mn[v];
+        } else {
+            int tm=(tl+tr)/2,ans=INF;
+            if (ql<=tm)
+                ans=min(ans,query_min(v*2,tl,tm,ql,qr));
+            if (qr> tm)
+                ans=min(ans,query_min(v*2+1,tm+1,tr,ql,qr));
+            return ans;
+        }
+    }
+    int query_max(int v, int tl, int tr, int ql, int qr) {
+        if (ql<=tl&&tr<=qr){
+            return mx[v];
+        } else {
+            int tm=(tl+tr)/2,ans=-INF;
+            if (ql<=tm)
+                ans=max(ans,query_max(v*2,tl,tm,ql,qr));
+            if (qr> tm)
+                ans=max(ans,query_max(v*2+1,tm+1,tr,ql,qr));
+            return ans;
+        }
+    }
+};
+
+int main() {
+    int N,M;cin>>N>>M;
+    segtree st_val(N+1),st_pos(N+1);
+    vector<pair<int,int>> p;
+    for(int i=1;i<=N;i++) {
+        int v;cin>>v;
+        st_val.update(1,1,N,i,v);
+        p.push_back({v,i});
+    }
+    sort(p.begin(),p.end());
+    for(int i=0;i<N;i++) {
+        st_pos.update(1,1,N,i+1,p[i].second);
+    }
+    for(int q=1;q<=M;q++){
+        string qt;cin>>qt;
+        if (qt=="S") {
+            int l,r;cin>>l>>r;
+            int lv=st_val.query_min(1,1,N,l,l),rv=st_val.query_min(1,1,N,r,r);
+            st_val.update(1,1,N,r,lv);
+            st_val.update(1,1,N,l,rv);
+        } else if (qt=="C"){
+            int l,r;cin>>l>>r;
+            int mn=st_val.query_min(1,1,N,l,r),mx=st_val.query_max(1,1,N,l,r);
+            printf("mn %d mx %d\n",mn,mx);
+            auto lit=lower_bound(p.begin(),p.end(),pair{mn,0});
+            if (lit!=p.begin()){
+                auto lp=prev(lit)-p.begin()+1;
+                int leftmost=st_pos.query_min(1,1,N,1,lp);
+                printf("left %d\n",leftmost);
+                if(leftmost<l){
+                    printf("No\n");
+                    continue;
+                }
+            }
+            auto rit=lower_bound(p.begin(),p.end(),pair{mn,INF});
+            if (rit!=p.begin()){
+                auto rp=prev(rit)-p.begin()+1;
+                int rightmost=st_pos.query_max(1,1,N,1,rp);
+                printf("right %d\n",rightmost);
+                if(rightmost>r){
+                    printf("No\n");
+                    continue;
+                }
+            }
+            cout<<"Yes\n";
+        }
+    }
+}
