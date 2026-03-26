c085931ee85f10181ac400ce6048fe659a4dd3bb
x5ilky <ethancai920@gmail.com>
2026-03-26 21:06:29 +1100

    stylistic choices

--- orac/fuel-station/f.cpp
+++ orac/fuel-station/f.cpp
@@ -12,22 +12,18 @@ struct S{
 };
 signed main() {
     using pii=pair<int,int>;
-    int N,D;cin>>N>>D;
-    N++;
+    int N,D;cin>>N>>D; N++;
     vector<S> s(N+1);
     for (int i=1;i<=N-1;i++) cin>>s[i].x>>s[i].a>>s[i].b;
     s[N].x=D;s[N].a=0;s[N].b=(1ull<<48);
     sort(s.begin()+1,s.end());
-    int sf=0,f=0;
-    set<pii>S;
+    int sf=0,f=0; set<pii>S;
     for (int i=1;i<=N;i++){
         auto [x,a,b]=s[i];
         while (f<x){
-            sf+=x-f;
-            f+=x-f;
+            sf+=x-f; f+=x-f;
             while (!S.empty()&&S.begin()->first<sf){
-                f-=S.begin()->second;
-                S.erase(S.begin());
+                f-=S.begin()->second; S.erase(S.begin());
             }
         }
         if (sf<=b){
