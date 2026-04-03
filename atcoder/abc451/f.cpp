53ce03f2bc73e1e37f74540be4ad486903ffaa61
x5ilky <ethancai920@gmail.com>
2026-04-03 20:56:10 +1100

    FIX

--- atcoder/abc451/f.cpp
+++ atcoder/abc451/f.cpp
@@ -4,49 +4,33 @@ using namespace std;
 
 int main() {
     int N,Q;cin>>N>>Q;
-    vector<int>HEAD(2*N+1),sz(2*N+1,1),col(2*N+1,0),fl(2*N+1,0),bl(2*N+1,0);
+    vector<int>HEAD(2*N+1),sz(2*N+1,1),col(2*N+1,0),bl(2*N+1,0);
     int ans=0,imp=false;
     for(int i=1;i<=2*N;i++)HEAD[i]=i;
+    for(int i=N+1;i<=2*N;i++)bl[i]=1;
 
     function<int(int)> head=[&](int n){
-        if(HEAD[n]==n){
-            if(fl[n])col[n]^=1,fl[n]=0;
-            return n;
-        }
-        int p=HEAD[n],r=head(p);
-        if(fl[n])col[n]^=1,fl[n]=0;
-        col[n]^=col[p];
-        return HEAD[n]=r;
-    };
-    function<int(int)> color=[&](int n){
-        return head(n),col[n];
-    };
-    function<void(int)> flip=[&](int u){
-        fl[u]^=1;
-        bl[u]=sz[u]-bl[u];
+        return HEAD[n]==n?n:HEAD[n]=head(HEAD[n]);
     };
     function<void(int,int)>join=[&](int u,int v){
-        int cu=color(u),cv=color(v);
         u=head(u),v=head(v);
         if(u==v)return;
-        if(sz[u]<sz[v])swap(u,v),swap(cu,cv);
-        if(cu==cv) flip(v);
+        if(sz[u]<sz[v])swap(u,v);
         sz[u]+=sz[v];
         bl[u]+=bl[v];
         HEAD[v]=u;
-        if(sz[u]-bl[u]<bl[u]) flip(u);
     };
     for (int i=1;i<=Q;i++){
         int u,v;cin>>u>>v;
         if(head(u)==head(v+N)||head(v)==head(u+N)){
-            ans-=bl[head(u)];
+            ans-=min(bl[head(u)],bl[head(u+N)]);
         } else {
-            ans-=bl[head(u)];
-            ans-=bl[head(v+N)];
+            ans-=min(bl[head(u)],bl[head(u+N)]);
+            ans-=min(bl[head(v)],bl[head(v+N)]);
         }
         join(u,v+N);
         join(v,u+N);
-        ans+=bl[head(u)];
+        ans+=min(bl[head(u)],bl[head(u+N)]);
 
         imp|=head(u)==head(v)||head(u+N)==head(v+N);
         cout<<(imp?-1:ans)<<endl;
@@ -55,26 +39,27 @@ int main() {
 
 // begin signature
 // +----------------------------------------+
-// |奏和労汎允山せ火ビビ丁丁ビ火山汎李耗義覇|
-// |汎せビ丁ミヘ一ㇸ・・　　ㇸㇵシビ允労奏義|
-// |シ一　　・ㇵヘシシㇸ　・ㇸ・・シ火汎和陽|
-// |ㇸシ丁ビビせシヘシシシヘ一　一ヘミ山労群|
-// |ビ山汎洪洪允丁せ山允允山火ミㇵ・シ丁允和|
-// |允労耗奏洪火允労李和耗和李汎せミㇸㇵビ允|
-// |洪耗陽奏洪山汎允山　・・　洪李汎火ヘ・丁|
-// |労群奏李汎せビシㇸ一一ㇵヘヘヘ一せシヘ・|
-// |耗耗洪山丁　・シ・ㇸ・　　一ヘシシ一ヘ・|
-// |李允ビ　　ㇸシ　一一・　　ㇸ・ヘミミㇸ・|
-// |火ヘ　ㇸ・一一ㇵシミミミシヘㇸ一ミヘㇸヘ|
-// |ㇵ　・シㇸヘ・シ山汎允せミヘ・　ㇵシヘシ|
-// |　ㇸミヘ一ㇸヘㇸ丁允李労せビヘヘ火山せ丁|
-// |　・ミ一シㇵㇸヘ　シ火允洪洪山丁火允允火|
-// |　一　ㇸミ山シ　ㇵ一ㇸシ丁ビ丁火火ビビビ|
-// |　ヘヘ　シせ允丁ㇵ　一一　・・ㇸシせヘㇸ|
-// |ㇸ丁一ヘ・シ火允火ミㇵ一一ㇵシビ山允ミヘ|
-// |シヘㇵビシ一シビせ山火火火せ允労和山允せ|
-// |ミㇸビ允火ビ丁ミ丁ビせ允労和奏群労和李允|
-// |シヘ山李労汎汎汎汎労李耗陽慶覇耗陽義耗允|
+// |奏和労洪汎山せビビビビ丁ビ火山汎李奏義覇|
+// |汎山火丁ミヘ一ㇸ・・・・ㇸㇵミ火允労奏慶|
+// |ミㇵ・　・ㇵシシシㇸ・・一・ㇸシ火汎和陽|
+// |ㇸヘミ丁火せシ一ㇸ一ㇸ・　一シヘミ山李群|
+// |ビせ允汎汎允丁丁ビ火火ビミㇵ　ㇵビ丁汎和|
+// |允労李洪山火・ㇸヘ　ㇵヘヘ一　ㇵ・シビ汎|
+// |洪和汎せ丁・・ㇵㇵ　　ㇸㇵシミシ一ㇸㇵ火|
+// |労洪せミ　　一シㇸㇵヘヘ一一ㇸシ丁ミㇸ一|
+// |労せシ　ㇸヘ・シ・丁山山火ミㇸ一ヘ丁ヘ・|
+// |火ㇵ　ヘㇸㇸシ　ミ允和李汎火ヘ一ㇸㇸㇸ一|
+// |・ㇸ・一ヘ一一ヘせ李義奏労せヘ・ㇵミミヘ|
+// |ㇸㇸミビㇸヘ・シ山李群陽李山丁ミせ汎允ビ|
+// |　ミ山せヘ一ヘㇸ丁允李奏奏李山火洪李汎せ|
+// |ㇸ・丁汎火ㇵㇸヘ　シ火汎洪洪洪允允汎汎山|
+// |・ㇵ　ミ山山シ　ヘ一ㇸシ丁ビビ火火ビ火ビ|
+// |・丁ヘ　シせ允丁ㇵ　一一　・・ㇸシせシ一|
+// |一火ビヘ・シ火允火ミㇵ一一ㇵシビ山汎丁丁|
+// |丁汎山ビシ一シビせ山火火火せ允労和汎洪允|
+// |山和洪允火ビ丁ミ丁ビせ允労和奏群和奏耗汎|
+// |汎耗奏李労汎汎汎汎労李奏陽慶慶和陽覇耗李|
 // +----------------------------------------+
-// 2026 (April 3rd) 18:07:04
+// 2026 (April 3rd) 20:55:37
 // end signature
+
