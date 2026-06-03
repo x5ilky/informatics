67da047ed1c5912d50d7739a356db3573af261a1
x5ilky <ethancai920@gmail.com>
2026-06-03 19:38:29 +1000

    tutor prodS

---
+++ orac/greed/g.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int R,C,K,H,V;cin>>R>>C>>K>>H>>V;
+    vector<int>h(H+1),v(V+1);
+    for(int i=1;i<=H;i++)cin>>h[i];
+    for(int i=1;i<=V;i++)cin>>v[i];
+    int lo=1,hi=R*C;
+    while(lo+1<hi){
+        int mid=(lo+hi)/2;
+        
+    }
+}
