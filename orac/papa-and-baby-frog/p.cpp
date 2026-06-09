c8002c6cfae804e9dc3b312554e5d396fb9b1692
x5ilky <ethancai920@gmail.com>
2026-06-09 12:54:00 +1000

    papa frog

--- orac/papa-and-baby-frog/p.cpp
+++ orac/papa-and-baby-frog/p.cpp
@@ -73,5 +73,116 @@ struct segtree{
 };
 int main() {
     int N,K;cin>>N>>K;
-    
+    vector<int>A(N+1);
+    for(int i=1;i<=N;i++)cin>>A[i];
+    vector<vector<int>>up(N+1),down(N+1);
+    vector<vector<int>>par(N+1,vector<int>(18));
+    using pii=pair<int,int>;
+    {
+        set<pii>byval,byidx;
+        for(int i=1;i<=N;i++){
+            byval.insert({A[i],i});
+            byidx.insert({i,A[i]});
+        }
+        while(byidx.size()){
+            auto v=*byval.begin();
+            vector<pii>vs;
+            while(byval.size()&&byval.begin()->first==v.first){
+                pii val=*byval.begin();
+                vs.push_back(val);
+                byidx.erase({val.second,val.first});
+                byval.erase(byval.begin());
+            }
+            for(auto [v,i]:vs){
+                int d=2e9,j=-1;
+                auto aft=byidx.upper_bound({i,v});
+                if(aft!=byidx.end()){
+                    d=aft->first-i;
+                    j=aft->first;
+                }
+                if(aft!=byidx.begin()){
+                    auto bef=prev(aft);
+                    if(i-bef->first<d){
+                        j=bef->first;
+                        d=i-bef->first;
+                    }
+                }
+                if(j!=-1){
+                    up[j].push_back(i);
+                    par[i][0]=j;
+                    // printf("up %d -> %d\n",i,j);
+                }
+            }
+        }
+    }
+    {
+        set<pii>byval,byidx;
+        for(int i=1;i<=N;i++){
+            byval.insert({A[i],i});
+            byidx.insert({i,A[i]});
+        }
+        while(byidx.size()){
+            auto v=*byval.rbegin();
+            vector<pii>vs;
+            while(byval.size()&&byval.rbegin()->first==v.first){
+                pii val=*byval.rbegin();
+                vs.push_back(val);
+                byidx.erase({val.second,val.first});
+                byval.erase(val);
+            }
+            for(auto [v,i]:vs){
+                int d=2e9,j=-1;
+                auto aft=byidx.upper_bound({i,v});
+                if(aft!=byidx.end()){
+                    d=aft->first-i;
+                    j=aft->first;
+                }
+                if(aft!=byidx.begin()){
+                    auto bef=prev(aft);
+                    if(i-bef->first<d){
+                        j=bef->first;
+                        d=i-bef->first;
+                    }
+                }
+                if(j!=-1){
+                    down[j].push_back(i);
+                    // printf("down %d -> %d\n",i,j);
+                }
+            }
+        }
+    }
+    vector<int>tin(N+1),tout(N+1),seen(N+1),depth(N+1,1);int t=0;
+    function<void(int)>dfs=[&](int u){
+        seen[u]=true;tin[u]=++t;
+        for(auto v:down[u])depth[v]=depth[u]+1,dfs(v);
+        tout[u]=t-1;
+    };
+    vector<pii>byhdown;
+    for(int i=1;i<=N;i++)byhdown.push_back({A[i],i});
+    sort(byhdown.begin(),byhdown.end());
+    for(int i=0;i>=N-1;i--){
+        int u=byhdown[i].second;
+        if(!seen[u])dfs(u);
+    }
+    segtree st(0,N+1,0,N+1);
+    vector<bool>good(N+1);
+    function<void(int,int)>dfs2=[&](int u,int d){
+        if(st.query(tin[u],depth[u],tout[u],depth[u]+K)){
+            good[u]=true;
+        }
+        st.set(tin[u],d,1);
+        for(auto v:up[u]){
+            dfs2(v,d+1);
+        }
+        st.set(tin[u],d,0);
+    };
+    fill(seen.begin(),seen.end(),0);
+    for(int i=0;i<N;i++){
+        int u=byhdown[i].second;
+        if(!seen[u])dfs2(u,1);
+    }
+    for(int i=1;i<=N;i++){
+        cout<<(int)good[i];
+    }
+    cout<<endl;
 }
