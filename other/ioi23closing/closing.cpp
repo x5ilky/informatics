b0044330d9065f7dc8223c68d86e4ae1283d0a9c
x5ilky <ethancai920@gmail.com>
2026-07-31 20:20:43 +1000

    ioi23closing

--- other/ioi23closing/closing.cpp
+++ other/ioi23closing/closing.cpp
@@ -10,9 +10,11 @@
 // precompute most score <= K with: disjoint, b -> st(A), a -> st(B)
 #include "closing.h"
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
-int max_score(int N, int X, int Y, long long K,
-              std::vector<int> U, std::vector<int> V, std::vector<int> W)
+const int INF=1ull<<60;
+signed max_score(signed N, signed X, signed Y, long long K,
+              std::vector<signed> U, std::vector<signed> V, std::vector<signed> W)
 {
     ++X,++Y;
     using pii=pair<int,int>;
@@ -48,17 +50,34 @@ int max_score(int N, int X, int Y, long long K,
     dA[X]=0,dfsd(X,-1,dA);dB[Y]=0,dfsd(Y,-1,dB);
     map<int,int>M__,Ma_,M_b,Mab;
     auto precomp=[&](int pA,int pB,map<int,int>&M){
-        priority_queue<int,vector<int>,greater<int>>pq;
+        vector<int>a(1),b(1);
         for(int i=1;i<=N;i++){
             if(isl[i])continue;
-            if(dA[i]!=-1)pq.push(dA[i]+pA);
-            if(dB[i]!=-1)pq.push(dB[i]+pB);
+            auto [d,p]=min<pii>({dA[i],pA},{dB[i],pB});
+            (p?b:a).push_back(d+p);
         }
-        int C=0,k=0;M[k]=C;
-        while(pq.size()){
-            auto w=pq.top();pq.pop();
-            M[k+=w]=++C;
+        sort(a.begin()+1,a.end());
+        sort(b.begin()+1,b.end());
+        for(int i=1;i<a.size();i++)a[i]+=a[i-1];
+        for(int i=1;i<b.size();i++)b[i]+=b[i-1];
+
+        int A=a.size()-1,B=b.size()-1;
+        vector<int>dp(A+2*B+1,INF);
+        for(int r=0;r<=1;r++){
+            int y=0;
+            for(int s=r;s<=A+2*B;s+=2){
+                int l=max(0ll,(s>A?(s-A+1)/2:0));
+                int h=min(B,s/2);
+                if(l>h)continue;
+                y=min(max(y,l),h);
+                while(y<h&&b[y+1]+a[s-2*(y+1)]<=b[y]+a[s-2*y])y++;
+                dp[s]=b[y]+a[s-2*y];
+            }
         }
+        for(int s=0;s<=A+2*B;s++)
+            if(dp[s]!=INF)M[dp[s]]=max(M[dp[s]],s);
+        int mx=0;
+        for(auto &[c,s]:M)s=mx=max(mx,s);
     };
     precomp(0,0,M__);
     precomp(dB[X],0,Ma_);
@@ -72,10 +91,11 @@ int max_score(int N, int X, int Y, long long K,
     for(int i=1;i<=M;i++)psA[i]=dA[ln[i]],psB[i]=dB[ln[i]];
     for(int i=1;i<=M;i++)if(!tp&&psA[i]>psB[i])tp=i;
     int ans=0,C=0;
-    priority_queue<int,vector<int>,greater<int>>pq;
-    for(int i=1;i<=M;i++)pq.push((i<tp?psA:psB)[i]);
+    priority_queue<pii,vector<pii>,greater<pii>>pq;
+    for(int i=1;i<=M;i++)pq.push({(i<tp?psA:psB)[i],i});
     bool rA=false,rB=false,flip=false;
-    for(int i=2;i<=2*M;i++){
+    int c=0;
+    while(true){
         map<int,int>*m=&M__;
         if(rA)m=&Ma_;
         if(rB)m=&M_b;
@@ -83,17 +103,49 @@ int max_score(int N, int X, int Y, long long K,
         int p=0;
         {
             auto it=m->upper_bound(K-C);
-            if(it==m->begin())p=0;
-            else p=(--it)->second;
+            if(it!=m->begin()) p=(--it)->second;
         }
-        ans=max(ans,i+p);
-        if(!flip&&pq.empty()){
+        ans=max(ans,c+p);
+        if(pq.empty()){
+            if(flip)break;
             flip=true;
-            for(int i=1;i<=M;i++)pq.push((i>tp?psA:psB)[i]-(i<tp?psA:psB)[i]);
+            for(int i=1;i<=M;i++)pq.push({(i>=tp?psA:psB)[i]-(i<tp?psA:psB)[i], i});
         }
-        if(flip&&pq.top()==1)rA=true;
-        if(flip&&pq.top()==M)rB=true;
-        C+=pq.top();pq.pop();
+        if(C+pq.top().first>K)break;
+        C+=pq.top().first;
+        c++;
+        if(flip&&pq.top().second==1)rA=true;
+        if(flip&&pq.top().second==M)rB=true;
+        pq.pop();
     } 
     return ans;
 }
+
+// begin signature
+// +----------------------------------------+
+// |ヘヘヘシミ丁ビ丁丁丁ミミ丁丁ビ火せ允洪李|
+// |・・・ㇸ一ヘヘㇵ　・・・ㇵヘシミビせ允洪|
+// |一一ㇸ・　ㇸヘミミ丁丁ミシㇵ・ヘミビ山允|
+// |ミシ一ㇵミ丁ビビビビビビビ丁ヘ・ヘ丁ビ火|
+// |シ一ヘ丁ビ丁ミ丁丁ビビ丁丁ビミㇵ・ㇵヘミ|
+// |シ一ミビ丁ミビ火せ火火ビ火ビミヘ一・・ㇸ|
+// |シヘ丁ビシ丁火ビ丁ミㇸヘシㇵ・　　　　　|
+// |ミビ丁丁丁ビ丁ミ一ヘシシシ一　一ㇵㇵㇸ・|
+// |火ビ火火火丁ヘ丁せ山山山允火ミㇵヘミヘ・|
+// |山允山ビシせ汎労労労労労汎山ビシシビヘ・|
+// |汎せ丁せ洪労労労労労労洪允火丁シ丁ミ一・|
+// |せせ汎労李労洪汎允允汎山せ火ミシミㇵ　　|
+// |洪允労労汎允山火火火火火ビビ丁シㇵㇸ　ㇸ|
+// |洪洪労洪允山山允允山山允允山せ火丁ミヘㇸ|
+// |和和李労洪允洪労労労洪労汎せビミシヘㇵヘ|
+// |群群群耗和労李和耗耗和労允火ミヘ一・　　|
+// |慶慶義陽奏和奏群陽奏李汎せ丁ヘ・・一ㇵㇵ|
+// |覇覇覇義奏群義義奏李汎せ丁ㇵ　一シミ丁ミ|
+// |覇覇覇陽群慶慶群和洪山丁ヘ　ㇵミ火せ火丁|
+// |覇覇慶群義覇陽耗労允火シㇸㇸシ火允汎せ丁|
+// +----------------------------------------+
+// 2026 (July 31st) 16:04:09
+// end signature
+
+
+
